cdd0c231c79e4df1233ec5a0e1ecd577ab80e089
NANDIMANDALAM-SUNNY <r170911@rguktrkv.ac.in>
2023-03-25 14:04:22 +0530

    changessss

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
- res.send("Hello World")
+ res.jso0n({
+  name: 'home',	
+ })
 });
 
 module.exports = router;
