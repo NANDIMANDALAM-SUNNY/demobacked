7840a625e1f6f1545c617bc48469b9eb85d7614b
NANDIMANDALAM-SUNNY <r170911@rguktrkv.ac.in>
2023-03-25 14:39:53 +0530

    majr

---
+++ Models/user.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const validator = require('validator')
+
+
+const userSchema = mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        validate:(value)=>{
+            return validator.isEmail(value)
+        }
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    profile:{
+        type:String,
+        required:true,
+    },
+   
+    verificationToken:{
+        type:String,
+    },
+    isVerified:{
+        type:Boolean,
+        default:false,
+    },
+    forgotPasswordToken:{
+        type:String 
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now(),
+        expireAfterSeconds: 30 
+    },
+})
+
+module.exports = mongoose.model('user',userSchema);
--- routes/users.js
+++ routes/users.js
@@ -1,22 +1,24 @@
 var express = require('express');
+const { Register ,login, confirmAccount, forgotPassword, resetPassword, newPassword, getProfile} = require('../controllers/users');
+const { validate } = require('../middleware/middleware');
 var router = express.Router();
 
-/* GET users listing. */
+
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('Hello World from backend');
 });
 
 
-router.post('/post',async (req,res)=>{
-try {
-  res.json({
-    profile:req.body
-  })
-} catch (error) {
-  res.json({
-    error
-  })
-}
-})
+
+router.post('/register', Register) 
+router.post('/login', login)
+router.get('/confirmAccount/:confirmationToken', confirmAccount)
+router.post('/forgot-password', forgotPassword)
+router.get('/reset-password/:token', resetPassword)
+router.post('/new-password/:token', newPassword)
+router.get('/get-profile',validate , getProfile)
+
+
+
 
 module.exports = router;
