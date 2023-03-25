11b3f3dff7b30d3b07a8073435bbae4bba81115b
NANDIMANDALAM-SUNNY <r170911@rguktrkv.ac.in>
2023-03-25 14:18:53 +0530

    ddf

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,17 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+
+router.post('/post',async (req,res)=>{
+try {
+  res.json({
+    profile:req.body
+  })
+} catch (error) {
+  res.json({
+    error
+  })
+}
+})
+
 module.exports = router;
